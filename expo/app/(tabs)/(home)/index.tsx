2c2b630604f5b31579ff4252712dfb21bfa79573
Kesi Umashankar <kesiumashankar@Kesis-MacBook-Pro.local>
2026-03-19 19:08:25 -0500

    inline uuid generation

--- expo/app/(tabs)/(home)/index.tsx
+++ expo/app/(tabs)/(home)/index.tsx
@@ -388,7 +388,7 @@ export default function HomeScreen() {
 
     const eventName = connectedEvents[0]?.name ?? null;
     const sessionId = Crypto.randomUUID();
-    const trackingToken = require('uuid').v4();
+    const trackingToken = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => { const r = Math.random() * 16 | 0; return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16); });
     setActiveSessionId(sessionId);
     setActiveTrackingToken(trackingToken);
     startSession(selectedEta, eventName);
